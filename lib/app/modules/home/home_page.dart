4a56cd80831ee9356210812f76eaf7a458c270e8
renan vargas <renanspvargas@outlook.com>
2023-09-12 07:53:28 -0300

    feat: home page navigation bar

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,16 +1,46 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:magazord_app_flutter/app/modules/todo_list/todo_list_router.dart';
 import './home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
-    
-    const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('HomePage'),),
-            body: Container(),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Navigator(
+          initialRoute: TodoListRouter.routeName,
+          key: Get.nestedKey(HomeController.navigatorKey),
+          onGenerateRoute: controller.onGeneratedRoute,
+        ),
+      ),
+      bottomNavigationBar: Obx(() {
+        return BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: (value) => controller.tabIndex = value,
+          currentIndex: controller.tabIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'ToDo',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.cloud),
+              label: 'Weather',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              label: 'Products',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
         );
-    }
+      }),
+    );
+  }
 }
