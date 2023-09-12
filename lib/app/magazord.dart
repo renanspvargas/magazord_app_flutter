0587f82486df5a7b97fd0f7acef1b01bf407a096
renan vargas <renanspvargas@outlook.com>
2023-09-12 07:26:40 -0300

    feat: add rest client bindings

--- lib/app/magazord.dart
+++ lib/app/magazord.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:magazord_app_flutter/app/magazord_bindings.dart';
 
 class Magazord extends StatelessWidget {
   const Magazord({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       title: 'Magazord App',
+      initialBinding: MagazordBindings(),
     );
   }
 }
--- lib/app/magazord_bindings.dart
+++ lib/app/magazord_bindings.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:magazord_app_flutter/app/core/rest_client/rest_client.dart';
 
 class MagazordBindings implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(RestClient(), permanent: true);
+  }
 }
