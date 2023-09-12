4a1ca15b82954d3546879cb3b4cd32789796b132
renan vargas <renanspvargas@outlook.com>
2023-09-12 08:26:40 -0300

    feat: logic for settings controller

--- lib/app/modules/settings/settings_controller.dart
+++ lib/app/modules/settings/settings_controller.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SettingsController extends GetxController {}
+class SettingsController extends GetxController {
+  void toggleTheme() {
+    Get.changeThemeMode(Get.isDarkMode ? ThemeMode.light : ThemeMode.dark);
+  }
+}
