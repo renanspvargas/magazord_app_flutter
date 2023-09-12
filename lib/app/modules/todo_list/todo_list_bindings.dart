294989bcdada7847cfdc84d0b2f16b59ca135a21
renan vargas <renanspvargas@outlook.com>
2023-09-12 08:01:40 -0300

    feat: add todo repo to bindings

--- lib/app/modules/todo_list/todo_list_bindings.dart
+++ lib/app/modules/todo_list/todo_list_bindings.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
+import 'package:magazord_app_flutter/app/repositories/todo_list/todo_list_repo.dart';
+import 'package:magazord_app_flutter/app/repositories/todo_list/todo_list_repo_impl.dart';
 import './todo_list_controller.dart';
 
 class TodoListBindings implements Bindings {
-    @override
-    void dependencies() {
-        Get.put(TodoListController());
-    }
+  @override
+  void dependencies() {
+    Get.put<TodoListRepo>(TodoListRepoImpl());
+    Get.put(TodoListController(repo: Get.find()));
+  }
 }
--- lib/app/modules/todo_list/todo_list_controller.dart
+++ lib/app/modules/todo_list/todo_list_controller.dart
@@ -1,3 +1,8 @@
 import 'package:get/get.dart';
+import 'package:magazord_app_flutter/app/repositories/todo_list/todo_list_repo.dart';
 
-class TodoListController extends GetxController {}
+class TodoListController extends GetxController {
+  final TodoListRepo _repo;
+
+  TodoListController({required TodoListRepo repo}) : _repo = repo;
+}
