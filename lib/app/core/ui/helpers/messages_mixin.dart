7a61b39a3a2b3a1b7127fde20d5f9226f9e95427
renan vargas <renanspvargas@outlook.com>
2023-09-12 08:07:01 -0300

    feat: create message mixin

---
+++ lib/app/core/ui/helpers/messages_mixin.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+mixin MessagesMixin on GetxController {
+  void messageListener(Rxn<MessageModel> message) {
+    ever<MessageModel?>(message, (model) async {
+      if (model != null) {
+        Get.snackbar(model.title, model.message,
+            backgroundColor: model.type.color(),
+            colorText: model.type.textColor(),
+            margin: const EdgeInsets.all(20));
+      }
+    });
+  }
+}
+
+class MessageModel {
+  final String title;
+  final String message;
+  final MessageType type;
+  MessageModel({
+    required this.title,
+    required this.message,
+    required this.type,
+  });
+}
+
+enum MessageType { error }
+
+extension MessageTypeColorExt on MessageType {
+  Color color() {
+    switch (this) {
+      case MessageType.error:
+        return Colors.red[800]!;
+    }
+  }
+
+  Color textColor() {
+    switch (this) {
+      case MessageType.error:
+        return Colors.white;
+    }
+  }
+}
