6c54dd0e5dd0abd4d9f19553fef4967b3c1ecc0a
renan vargas <renanspvargas@outlook.com>
2023-09-12 07:37:36 -0300

    feat: sealing home router

--- lib/app/modules/home/home_router.dart
+++ lib/app/modules/home/home_router.dart
@@ -2,9 +2,7 @@ import 'package:get/route_manager.dart';
 import 'package:magazord_app_flutter/app/modules/home/home_bindings.dart';
 import 'package:magazord_app_flutter/app/modules/home/home_page.dart';
 
-class HomeRouter {
-  HomeRouter._();
-
+sealed class HomeRouter {
   static const routeName = '/home';
 
   static final routers = <GetPage>[
