6fa3e2de5b1ba304ba51e7daf8b2053445e47f2a
renan vargas <renanspvargas@outlook.com>
2023-09-12 07:24:08 -0300

    feat: organizing main

---
+++ lib/app/magazord.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+class Magazord extends StatelessWidget {
+  const Magazord({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Magazord App',
+    );
+  }
+}
---
+++ lib/app/magazord_bindings.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class MagazordBindings implements Bindings {
+  @override
+  void dependencies() {}
+}
