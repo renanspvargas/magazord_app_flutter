63dfc630b98511b5212ec92790b90bd0e1c5b484
renan vargas <renanspvargas@outlook.com>
2023-09-12 08:27:42 -0300

    feat: finish settings page

--- lib/app/modules/settings/settings_page.dart
+++ lib/app/modules/settings/settings_page.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 import './settings_controller.dart';
 
 class SettingsPage extends GetView<SettingsController> {
-    
-    const SettingsPage({Key? key}) : super(key: key);
+  const SettingsPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('SettingsPage'),),
-            body: Container(),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text('Troque o tema da aplicação'),
+              ElevatedButton(
+                onPressed: () => controller.toggleTheme(),
+                child: const Text('Toggle Theme'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
