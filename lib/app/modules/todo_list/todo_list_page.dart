034f7f2da085ddf800f127b9ed81b21404aba12c
renan vargas <renanspvargas@outlook.com>
2023-09-12 08:04:10 -0300

    feat: finish todo list page

--- lib/app/modules/todo_list/todo_list_page.dart
+++ lib/app/modules/todo_list/todo_list_page.dart
@@ -1,16 +1,32 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:magazord_app_flutter/app/models/todo_list/todo_model.dart';
 import './todo_list_controller.dart';
 
 class TodoListPage extends GetView<TodoListController> {
-    
-    const TodoListPage({Key? key}) : super(key: key);
+  const TodoListPage({Key? key}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(title: const Text('TodoListPage'),),
-            body: Container(),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(() {
+        return ListView.builder(
+          itemCount: controller.tasks.length,
+          itemBuilder: (context, index) {
+            TodoModel task = controller.tasks[index];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2.0),
+              child: CheckboxListTile(
+                title: Text(task.title),
+                subtitle: Text(task.description),
+                value: task.completed,
+                onChanged: (value) => controller.didTapTask(value, index),
+                tileColor: task.completed ? Colors.grey : Colors.transparent,
+              ),
+            );
+          },
         );
-    }
+      }),
+    );
+  }
 }
