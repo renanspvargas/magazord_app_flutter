ab4c11848e37e8f1f7c4d8a087ffeb2c404d1db7
renan vargas <renanspvargas@outlook.com>
2023-09-12 08:21:49 -0300

    feat: products controller logic and bindings

--- lib/app/modules/products_list/products_list_bindings.dart
+++ lib/app/modules/products_list/products_list_bindings.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
+import 'package:magazord_app_flutter/app/repositories/products_list/products_list_repo.dart';
+import 'package:magazord_app_flutter/app/repositories/products_list/products_list_repo_impl.dart';
 import './products_list_controller.dart';
 
 class ProductsListBindings implements Bindings {
-    @override
-    void dependencies() {
-        Get.put(ProductsListController());
-    }
+  @override
+  void dependencies() {
+    Get.put<ProductsListRepo>(ProductsListRepoImpl());
+    Get.put(ProductsListController(repo: Get.find()));
+  }
 }
--- lib/app/modules/products_list/products_list_controller.dart
+++ lib/app/modules/products_list/products_list_controller.dart
@@ -1,3 +1,26 @@
 import 'package:get/get.dart';
+import 'package:magazord_app_flutter/app/models/products_list/product_model.dart';
+import 'package:magazord_app_flutter/app/repositories/products_list/products_list_repo.dart';
 
-class ProductsListController extends GetxController {}
+class ProductsListController extends GetxController {
+  final ProductsListRepo _repo;
+
+  ProductsListController({required ProductsListRepo repo}) : _repo = repo;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _getAllProducts();
+  }
+
+  final List<ProductModel> _products = <ProductModel>[].obs;
+  List<ProductModel> get products => _products;
+
+  void _getAllProducts() {
+    _products.addAll(_repo.getAllProducts());
+  }
+
+  void didTapProduct(int index) {
+    // TODO: details page
+  }
+}
